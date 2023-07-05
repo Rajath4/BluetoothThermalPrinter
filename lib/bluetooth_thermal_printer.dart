466db5551eca87c4b8b15fc1e4f933c0cdf42587
Nandish N S <92267208+nandishns@users.noreply.github.com>
2023-07-05 11:57:59 +0530

    added disconnect function in bluetooth_thermal_printer.dart

--- lib/bluetooth_thermal_printer.dart
+++ lib/bluetooth_thermal_printer.dart
@@ -46,6 +46,17 @@ class BluetoothThermalPrinter {
     }
     return result;
   }
+  // disconnect to any connected device                        --> new feature
+  static Future<String?> disconnect() async {
+    String? result = "false";
+    try {
+      result = await _channel.invokeMethod('disconnectPrinter');
+    } on PlatformException catch (e) {
+      print("Failed to disconnect: '${e.message}'.");
+    }
+    return result;
+  }
+
 
   ///Printes the [bytes] using bluetooth printer.
   static Future<String?> writeBytes(List<int> bytes) async {
